ba66f0ecb5bd603c5dd30f33715891cdea12d428
arms22 <arms22@gmail.com>
2020-10-06 20:24:38 +0900

    Pololu USB AVR Programmer v2

--- extras/burn-bootloader.sh
+++ extras/burn-bootloader.sh
@@ -1,19 +1,21 @@
 #! /usr/bin/env bash
 
-AVRROOT=/Applications/Arduino1.0.6.app/Contents/Resources/Java/hardware/tools/avr
+AVRROOT=/Applications/Arduino.app/Contents/Java/hardware/tools/avr
 SKETCH=dotsduino_demo.ino.hex
-BOOTLOADER=/Applications/Arduino1.0.6.app/Contents/Resources/Java/hardware/arduino/bootloaders/atmega/ATmegaBOOT_168_atmega328_pro_8MHz.hex
+BOOTLOADER=/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/bootloaders/atmega/ATmegaBOOT_168_atmega328_pro_8MHz.hex
+PROGRAMPORT=/dev/cu.usbmodem00297912
+ARDUINOPORT=/dev/cu.usbmodem00297914
 
 echo "######## Fuse set ########"
 sleep 1
-$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -v -v -v -patmega328p -cstk500v2 -Pusb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDA:m -Ulfuse:w:0xFF:m 
+$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -patmega328p -cstk500 -P$PROGRAMPORT -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xDA:m -Ulfuse:w:0xFF:m
 
 echo "######## Write Bootloader ########"
 sleep 1
-$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -v -v -v -patmega328p -cstk500v2 -Pusb -Uflash:w:$BOOTLOADER:i -Ulock:w:0x0F:m 
+$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -patmega328p -cstk500 -P$PROGRAMPORT -Uflash:w:$BOOTLOADER:i -Ulock:w:0x0F:m
 
 echo "######## Write Demo sketch ########"
 sleep 1
-$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -patmega328p -carduino -P/dev/cu.usbserial-A800csyY -b57600 -D -Uflash:w:$SKETCH:i
+$AVRROOT/bin/avrdude -C$AVRROOT/etc/avrdude.conf -v -patmega328p -carduino -P$ARDUINOPORT -b57600 -D -Uflash:w:dotsduino_demo.ino.hex:i
 
 echo $'\a'
